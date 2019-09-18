56c5371542d55c07cf0efafac73faa737d3c326b
DavidMonk00 <DavidMonk00@users.noreply.github.com>
2019-09-18 16:28:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@
 cat /root/.ssh/id_rsa.pub >> /cache/ssh/authorized_keys
 awk '!a[$0]++' /cache/ssh/authorized_keys
 
-python ./disk-usage.py
+python ./disk-usage.py $SSH_USERNAME
 
 tail -f /dev/null
